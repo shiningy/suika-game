dc5cbf1777ef4f41912df2e022c40501b553d7ff
shiningy84 <shiningy84@gmail.com>
2023-11-28 23:37:41 +0900

    first commit

---
+++ fruits.js
@@ -0,0 +1,48 @@
+const FRUITS_HLW = [
+    {
+        name: "halloween/00_cherry",
+        radius: 46 / 2,
+    },
+    {
+        name: "halloween/01_strawberry",
+        radius: 55 / 2,
+    },
+    {
+        name: "halloween/02_grape",
+        radius: 67 / 2,
+    },
+    {
+        name: "halloween/03_gyool",
+        radius: 81 / 2,
+    },
+    {
+        name: "halloween/04_orange",
+        radius: 97 / 2,
+    },
+    {
+        name: "halloween/05_apple",
+        radius: 117 / 2,
+    },
+    {
+        name: "halloween/06_pear",
+        radius: 142 / 2,
+    },
+    {
+        name: "halloween/07_peach",
+        radius: 171 / 2,
+    },
+    {
+        name: "halloween/08_pineapple",
+        radius: 206 / 2,
+    },
+    {
+        name: "halloween/09_melon",
+        radius: 249 / 2,
+    },
+    {
+        name: "halloween/10_watermelon",
+        radius: 300 / 2,
+    },
+];
+
+export { FRUITS_HLW };
